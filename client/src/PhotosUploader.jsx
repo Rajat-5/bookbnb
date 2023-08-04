bf9a8c8f9a2a13619268a6bbce60f6c51d054a37
Rajat-5 <110753977+Rajat-5@users.noreply.github.com>
2023-08-05 01:45:40 +0530

    Update PhotosUploader.jsx

--- client/src/PhotosUploader.jsx
+++ client/src/PhotosUploader.jsx
@@ -48,7 +48,7 @@ export default function PhotosUploader({addedPhotos,onChange}){
                 {addedPhotos.length > 0 && addedPhotos.map(link=> (
                     // eslint-disable-next-line react/jsx-key
                     <div className="h-32 flex relative" key={link}>
-                        <img className="rounded-2xl w-full object-cover" src={ 'http://localhost:4000/uploads/' +link} />
+                        <img className="rounded-2xl w-full object-cover" src={ 'https://bookbnb-rajat.onrender.com/uploads/' +link} />
                         <button onClick={event => removePhoto(event, link)} className="cursor-pointer absolute bottom-2 right-1 text-white bg-gray-300 bg-opacity-50 rounded-2xl p-1">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
